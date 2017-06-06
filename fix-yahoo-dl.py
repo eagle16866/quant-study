cdc58cd31a930982f770d16a26dd2fb50c7476bb
eagle16866 <jyhsieh@yahoo.com>
2017-06-06 11:58:49 +0800

    Add files via upload
    https://github.com/ranaroussi/fix-yahoo-finance

---
+++ fix-yahoo-dl.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 02 13:37:48 2017
+
+@author: jyhsi
+
+Using fix_yahoo_finance to download stock data
+"""
+
+from pandas_datareader import data as pdr
+import fix_yahoo_finance  # <== that's all it takes :-)
+
+ticker = "ibm"
+# Download dataframe
+data = pdr.get_data_yahoo(ticker, start="2010-01-01", end="2017-6-6")
+data.to_csv('D:\Workspace\download\%s.csv' %ticker)  
+print("Download completed!")
+# Pring last 5 rows 
+print(data.tail()) 
